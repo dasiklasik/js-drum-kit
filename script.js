2aa7ce53b17c0d7197d6a530103fc620c7f80486
dasiklasik <daryasamsonowicz@gmail.com>
2023-02-21 16:38:19 +0300

    feat: add button transtion

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-window.addEventListener('keydown', function (e) {
+const playSound = (e) => {
     const audio = document.querySelector(`audio[data-key=${e.key.toUpperCase()}]`)
+    const key = document.querySelector(`.key[data-key=${e.key.toUpperCase()}]`)
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
-})
+    key.classList.add('playing')
+}
+
+window.addEventListener('keydown', playSound)
+
+const keys = document.querySelectorAll('.key')
+
+keys.forEach(key => key.addEventListener('transitionend', (e) => {
+    if(e.propertyName === 'transform') {
+        key.classList.remove('playing')
+    }
+}))
+
