3f6b8bbcc73a295dec3ce58f37726929c37f8247
dasiklasik <daryasamsonowicz@gmail.com>
2023-02-21 16:03:05 +0300

    feat: add audio playback

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-const audio = document.querySelectorAll('audio')
-const keys = document.querySelectorAll('.key')
-
 window.addEventListener('keydown', function (e) {
-    keys.forEach(function (item, i, arr) {
-        if(item.dataset.key === e.key.toUpperCase()) {
-            item.classList.add('playing')
-            setTimeout(function () {item.classList.remove('playing')}, 100)
-        }
-    })
+    const audio = document.querySelector(`audio[data-key=${e.key.toUpperCase()}]`)
+    if (!audio) return;
+    audio.play();
 })
